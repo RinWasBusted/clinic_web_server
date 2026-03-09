3f252186eba8fb87435b7b01680331020fed64cf
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2026-03-09 11:46:51 +0700

    fix: update schema and refactor role handling for compatibility BREAKING CHANGES: - roleName instead of role in db schema. roleName is kept for older commits compatibility - add code field in Permission table to readable (business id code) - updated the schema to keep compatibility to older version TODOS: - rewrite the authorizeRole --> authorizedPermission

--- src/middlewares/role.ts
+++ src/middlewares/role.ts
@@ -1,38 +1,35 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../utils/prisma.js";
-type Role = "manager" | "staff" | "doctor" | "pharmacist"
+type Role = "manager" | "staff" | "doctor" | "pharmacist";
 const roleRank: Record<Role, number> = {
-    manager: 2,
-    staff: 1,
-    doctor: 0,
-    pharmacist: 0,
+  manager: 2,
+  staff: 1,
+  doctor: 0,
+  pharmacist: 0,
 };
 
-export const  checkRole = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const currentRole = req.user?.role as Role;
-    const targerRoleId = req.params.id ??"";
-    if (Array.isArray(targerRoleId)) {
-        return res.status(400).json({ message: "id must be a string, not an array" });
-    }
-    const targetUser = await prisma.account.findUnique({
-        where: { accountID: targerRoleId },
-        select: { role: true },
+// Change name to roleName for compatibility with old code.
+export const checkRole = async (req: Request, res: Response, next: NextFunction) => {
+  const currentRole = req.user?.role as Role;
+  const targerRoleId = req.params.id ?? "";
+  if (Array.isArray(targerRoleId)) {
+    return res.status(400).json({ message: "id must be a string, not an array" });
+  }
+  const targetUser = await prisma.account.findUnique({
+    where: { accountID: targerRoleId },
+    select: { roleName: true },
+  });
+  const targetRole = targetUser?.roleName as Role;
+  if (!currentRole || !targetRole) {
+    return res.status(400).json({ message: "Invalid role" });
+  }
+  if (roleRank[currentRole] <= roleRank[targetRole]) {
+    return res.status(403).json({
+      message: "Cannot operate on same or higher role",
     });
-    const targetRole = targetUser?.role as Role;
-    if (!currentRole || !targetRole) {
-        return res.status(400).json({ message: "Invalid role" });
-    }
-    if (roleRank[currentRole] <= roleRank[targetRole]) {
-        return res.status(403).json({
-            message: "Cannot operate on same or higher role",
-        });
-    }
-    next();
-}
+  }
+  next();
+};
 // check role for create account, only manager can create staff and manager, staff can only create doctor and pharmacist
 export const authorizeRoles = (...allowedRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
